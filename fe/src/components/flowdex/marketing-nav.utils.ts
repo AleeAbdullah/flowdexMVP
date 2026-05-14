d594814769e560d1fc1cd5ecd89998408ee66271
ali abdullah <spam.alee.abd@gmail.com>
2026-05-14 23:59:36 +0500

    Remove 'Buy' item from MARKETING_NAV_ITEMS in marketing-nav.utils.ts

--- fe/src/components/flowdex/marketing-nav.utils.ts
+++ fe/src/components/flowdex/marketing-nav.utils.ts
@@ -6,7 +6,6 @@ export type MarketingNavItem = {
 export const MARKETING_NAV_ITEMS: MarketingNavItem[] = [
   { label: 'About', href: '/about' },
   { label: 'Tokenomics', href: '/tokenomics' },
-  { label: 'Buy', href: '/buy' },
   { label: 'Roadmap', href: '/roadmap' },
   { label: 'Whitepaper', href: '/whitepaper' },
   { label: 'FAQ', href: '/faq' },
